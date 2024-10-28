723df5ebc6d8f611c61a140072fabdd424a063ec
MRY61986 <martin_rybicki@hotmail.com>
2024-10-28 18:17:29 -0500

    Martin's update Plots

---
+++ config.py
@@ -0,0 +1,8 @@
+# OpenWeatherMap API Key
+weather_api_key = "22cdc5a3dcff0746215eda643a012142"
+
+# Geoapify API Key
+geoapify_key = "a1ae7b8b70504f00864b7e3abdf72d6e"
+
+# US Census API Key
+census_key = "bf972bc91c4cb2594553ae9dfdf2507d653c97ec"
